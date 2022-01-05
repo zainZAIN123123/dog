3774779a775c5f3635390f2f488dd714494195e4
zainZAIN123123 <96651772+zainZAIN123123@users.noreply.github.com>
2022-01-05 13:37:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+menu_list_array = ["dynamite pizza",
+                        "bbq pizza",
+                        "ranch pizza",
+                        "double stuffed pizza",
+                        "cheese pizza",
+                        "extra cheesey pizza"];
+                        console.log("menu_list_array");
+    
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("menu_list_array").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
+    }
+
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata="menu_list_array.sort()";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
